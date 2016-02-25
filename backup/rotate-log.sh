7b9f1f88ece967be4e8fb9cc565ef0bf33f82a33
Dino Korah <dino.korah@redmatter.com>
2016-02-25 12:39:43 +0000

    Temporarily removing "backup"

--- backup/rotate-log.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-/bin/gzip -c ${BITBUCKET_BACKUP_LOG} > ${BITBUCKET_BACKUP_LOG}.$(date +%Y%m%d).gz &&
-	cat /dev/null > ${BITBUCKET_BACKUP_LOG}
--- backup/start-cron.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# setup a background subshell that would touch crontabs so that they are correctly loaded
-# work aroud for some quirks with vixie cron
-( sleep 5; touch $(find /var/spool/cron/crontabs) )&
-
-exec /usr/sbin/cron -f -L15
